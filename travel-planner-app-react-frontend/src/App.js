fab9ef0a2cf724876626bcb3e9280629ac1a3b3d
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-03-30 17:29:03 +0300

    #19

--- travel-planner-app-react-frontend/src/App.js
+++ travel-planner-app-react-frontend/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import Trips from "./pages/Trips";
 import Home from "./pages/Home";
+import Trips from "./pages/Trips";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="trips" element={<Trips />} />
+            <Route path="*" element={<Page404 />} />
           </Route>
         </Routes>
       </BrowserRouter>
