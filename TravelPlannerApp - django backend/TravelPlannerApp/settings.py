655b43fc21783ec6ca4b232b84b256d05995e627
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-04-02 23:48:26 +0300

    Update settings.py

--- TravelPlannerApp - django backend/TravelPlannerApp/settings.py
+++ TravelPlannerApp - django backend/TravelPlannerApp/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-t4sj5qz+rm6me!+e*k)o#$j7y6y(6(5n@#2k(%v=yuh5c*uklp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "172.30.117.16",
+    "192.168.93.102",
+    "127.0.0.1",
+    "34.106.170.165",
+    "34.106.108.42",
+]
 
 
 # Application definition
