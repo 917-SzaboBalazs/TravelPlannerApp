587447d2c56f35c14c699b7ef08e63e6c5c40fe8
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-04-18 17:16:12 +0300

    Update settings.py

--- TravelPlannerApp - django backend/TravelPlannerApp/settings.py
+++ TravelPlannerApp - django backend/TravelPlannerApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 import environ
 
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -134,7 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = '/var/www/TravelPlannerApp/TravelPlannerApp - django backend/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
