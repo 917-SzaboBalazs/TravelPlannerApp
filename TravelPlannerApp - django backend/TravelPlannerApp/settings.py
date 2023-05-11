3ccbd7e5b748b506340967c2a1639db927fb5d5e
Szabó Balázs <szabo.balazs2017@gmail.com>
2023-05-11 15:17:25 +0300

    settings.py

--- TravelPlannerApp - django backend/TravelPlannerApp/settings.py
+++ TravelPlannerApp - django backend/TravelPlannerApp/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if not DEBUG:
-    STATIC_ROOT = '/var/www/TravelPlannerApp/TravelPlannerApp - django backend/static/'
+    STATIC_ROOT = '/home/User/TravelPlannerApp/TravelPlannerApp - django backend/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
